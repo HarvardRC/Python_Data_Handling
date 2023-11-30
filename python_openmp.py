7910292b732488ecc0be159cfe2123844438d624
samahkarim <samah_karim@g.harvard.edu>
2023-11-30 14:50:10 -0500

    add comments to openmp script

--- python_openmp.py
+++ python_openmp.py
@@ -2,17 +2,27 @@ import os
 from time import time
 import numpy as np
 
-print('Using %d processors' % int(os.getenv('SLURM_CPUS_PER_TASK',1)))
+# Print the number of processors being used (default to 1 if not specified)
+print("Using %d processors" % int(os.getenv("SLURM_CPUS_PER_TASK", 1)))
 
+# Define the number of rounds for the loop
 nrounds = 5
 
+# Record the start time for performance measurement
 t_start = time()
 
+# Loop through the specified number of rounds
 for i in range(nrounds):
-    a = np.random.random([2000,2000])
-    a = a + a.T
-    b = np.linalg.pinv(a) # compute (Moore-Penrose) pseudo-inverse of a matrix
+    # Generate a random symmetric matrix of size 2000x2000
+    a = np.random.random([2000, 2000])
+    a = a + a.T  # Make the matrix symmetric by adding its transpose
+    # Compute the (Moore-Penrose) pseudo-inverse of the matrix
+    b = np.linalg.pinv(a)
 
+# Calculate the total time taken for the specified number of rounds
 t_delta = time() - t_start
 
-print('Seconds taken to invert %d symmetric 2000x2000 matrices: %f' % (nrounds, t_delta))
+# Print the time taken to invert the specified number of symmetric 2000x2000 matrices
+print(
+    "Seconds taken to invert %d symmetric 2000x2000 matrices: %f" % (nrounds, t_delta)
+)
